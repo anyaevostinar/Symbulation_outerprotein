90b5dfbe70bac211491ce1674a60dfbf7f02efc7
Anya Johnson <anyaejo@gmail.com>
2019-01-03 18:09:14 -0500

    fixed up main

--- SymbulationEmp/source/native/symbulation.cc
+++ SymbulationEmp/source/native/symbulation.cc
@@ -11,15 +11,14 @@ EMP_BUILD_CONFIG( SymConfigBase,
                  VALUE(MUTATION_RATE, double, 0.002, "Standard deviation of the distribution to mutate by"),
                  VALUE(SYNERGY, double, 5, "Amount symbiont's returned resources should be multiplied by"),
                  VALUE(VERTICAL_TRANSMISSION, double, 0.1, "Value 0 to 1 of probability of symbiont vertically transmitting when host reproduces"),
-		  VALUE(HOST_INT, double, 0, "Interaction value from -1 to 1 that hosts should have initially, -2 for random"),
-		  VALUE(SYM_INT, double, 0, "Interaction value from -1 to 1 that symbionts should have initially, -2 for random"),
+		  VALUE(HOST_INT, double, -2, "Interaction value from -1 to 1 that hosts should have initially, -2 for random"),
+		  VALUE(SYM_INT, double, -2, "Interaction value from -1 to 1 that symbionts should have initially, -2 for random"),
                   VALUE(WORLD_TYPE, int, 1, "Type of the world (1 for Mixed and 2 for Grid"),
-                 VALUE(GRID_X, int, 5, "Width of the world"),
-                 VALUE(GRID_Y, int, 5, "Height of world"),
-                 VALUE(UPDATES, int, 1, "Number of updates to run before quitting"),
-                          
-                 )
-//TODO: add option for random host and sym int values
+                 VALUE(POP_X, int, 5, "Width of the world"),
+                 VALUE(POP_Y, int, 5, "Height of world"),
+		  VALUE(UPDATES, int, 1, "Number of updates to run before quitting"),
+		  VALUE(NAME, string, "CHANGEME", "Name for the data files, recommended to be the values being tested"))
+
  
 	 
 int main(int argc, char * argv[])
@@ -36,8 +35,8 @@ int main(int argc, char * argv[])
     if (args.TestUnknown() == false) exit(0); //Leftover args no good
 
     double numupdates = config.UPDATES();
-    //double POP_SIZE = config.GRID_X() * config.GRID_Y();
-    double POP_SIZE = 400;
+    double POP_SIZE = config.POP_X() * config.POP_Y();
+    
     bool random_phen_host = false;
     bool random_phen_sym = false;
     if(config.HOST_INT() == -2) random_phen_host = true;
@@ -46,80 +45,45 @@ int main(int argc, char * argv[])
     emp::Random random(config.SEED());
         
     SymWorld world(random); 
-    //world.SetPopStruct_Grid(config.GRID_X(), config.GRID_Y();  
-    world.SetPopStruct_Mixed(false);
+
+    if (config.WORLD_TYPE() == 1){
+      world.SetPopStruct_Mixed();
+    } else if (config.WORLD_TYPE() == 2) {
+      world.SetPopStruct_Grid(config.POP_X(), config.POP_Y());
+    }
+
     //world.SetVertTrans(config.VERTICAL_TRANSMISSION());    
     world.SetMutRate(config.MUTATION_RATE());
     //Set up files
     world.SetupPopulationFile().SetTimingRepeat(10);
-    world.SetupHostIntValFile("HostVals_"+to_string(POP_SIZE)+"_"+to_string(config.SEED())+"_"+to_string(config.VERTICAL_TRANSMISSION())+".data").SetTimingRepeat(10);
-    world.SetupSymIntValFile("SymVals_"+to_string(POP_SIZE)+"_"+to_string(config.SEED())+"_"+to_string(config.VERTICAL_TRANSMISSION())+".data").SetTimingRepeat(10);
+    world.SetupHostIntValFile("HostVals_"+config.NAME()+".data").SetTimingRepeat(10);
+    world.SetupSymIntValFile("SymVals_"+config.NAME()+".data").SetTimingRepeat(10);
 
-    //inject organism
+    //inject organism and symbiont
     for (size_t i = 0; i < POP_SIZE; i++){
       Symbiont new_sym; 
       Host *new_org;
-      if(random_phen_sym) {
+      if(config.SYM_INT() == -2) {
         new_sym = *(new Symbiont(random.GetDouble(-1, 1)));
         new_sym.SetVertTrans(config.VERTICAL_TRANSMISSION());
       }
       else {
         new_sym = *(new Symbiont(config.SYM_INT()));   
-        //Added
         new_sym.SetVertTrans(config.VERTICAL_TRANSMISSION());
-        //std::cout<< "hey "<< new_sym.GetVertTrans();
       }
-      //std::cout<< "hey "<< new_sym.GetVertTrans();
-      if (random_phen_host) new_org = new Host(random.GetDouble(-1, 1), new_sym);
-      else new_org = new Host(config.HOST_INT(), new_sym);
-
-      //Added
-      new_org->SetOuterProteins({"111"});
       new_sym.SetInjectors({"111"});
+      //TODO: currently sym always succeeds, should it have the possibility of failing based on proteins?
+      if (config.HOST_INT() == -2) new_org = new Host(random.GetDouble(-1, 1), new_sym);
+      else new_org = new Host(config.HOST_INT(), new_sym);
+      new_org->SetOuterProteins({"111"});
 
-      //std::vector<std::string> v=new_org->GetOuterProteins();
-     
-            
-      // for(int i=0; i<v.size(); i++){
-      //    cout << v.at(i) << " ";
-      //}
-     
-    
-      // Host *new_org = new Host(config.HOST_INT(), *(new Symbiont(config.SYM_INT())));
-         
-      //search for specific receptor
-      std::vector<std::string> hostproteins = new_org->GetOuterProteins();
-      std::vector<std::string> injectors = new_sym.GetInjectors();
-      bool through = false;
-      // int count=0;
-      
-  
-      for(int i=0; i<hostproteins.size(); i++){
-        for(int j=0; j<injectors.size(); j++){
-          if(hostproteins.at(i)==injectors.at(j)){
-            //std::cout << injectors.at(j)<<"\n";
-            //count++;
-            through=true;
-            //std::cout << count<< "\n";
-          }
-        }
-      }   
- 
-      if(through == true){
-        world.Inject(*new_org);
-      } 
-
-      else{
-        std::cout<< "No receptors; injection failed\n";
-      }
-
-      
+      world.Inject(*new_org);
     }
 
     //Loop through update
       
     for (int i = 0; i < numupdates; i++) {
-      //cout << i << endl;
+      cout << i << endl;
       world.Update();
     }
 
